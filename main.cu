5d583dd1baf2c9e42fe025cf34015e7aae97b836
bunnyDrug <1652666+bunnyDrug@users.noreply.github.com>
2023-02-05 23:44:09 +0000

    Problem one solved on the GPU
    - Need to attempt a parallel reduction.
    - Profile application (unable to do so at time of writing as WSL doesn't support profiling)
    - Look and understand stride and thread cooperation.

---
+++ main.cu
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cuda_runtime.h>
+#include <numeric>
+
+using namespace std;
+
+__global__
+void gpuFactor(uint *x) {
+    uint tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if (tid % 3 == 0 || tid % 5 == 0) {
+        x[tid] = tid;
+    }
+}
+
+int main() {
+
+    int LIMIT = 1000;
+
+    // allocate memory on the GPU
+    uint *device_a;
+    cudaMalloc((void **) &device_a, LIMIT * sizeof(int));
+
+    // run kernel
+    gpuFactor<<<5, 200>>>(device_a);
+
+    // Copy memory from GPU back to CPU and store it in host_a
+    int host_a[LIMIT];
+    cudaMemcpy(host_a, device_a, LIMIT * sizeof(int), cudaMemcpyDeviceToHost);
+
+    // Run accumulate sum on CPU
+    // TODO parallel reduce on GPU.
+    cout << accumulate(host_a, host_a + LIMIT, 0) << endl;
+
+}
