c382a08e5d95b52042806e8d7ace5d15197b4b02
bunnyDrug <1652666+bunnyDrug@users.noreply.github.com>
2023-02-07 21:54:48 +0000

    QOL updates

--- main.cu
+++ main.cu
@@ -4,10 +4,13 @@
 
 using namespace std;
 
+#define LIMIT 1000
+
 __global__
 void gpuFactor(uint *x) {
     uint tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid == 0) { return; }
+
+    if (tid == 0 || tid > LIMIT) { return; }
 
     if (tid % 3 == 0 || tid % 5 == 0) {
         x[tid] = tid;
@@ -16,21 +19,21 @@ void gpuFactor(uint *x) {
 
 int main() {
 
-    int LIMIT = 1000;
-
     // allocate memory on the GPU
     uint *device_a;
     cudaMalloc((void **) &device_a, LIMIT * sizeof(int));
 
     // run kernel
-    gpuFactor<<<5, 200>>>(device_a);
+    gpuFactor<<<5, 256>>>(device_a);
 
     // Copy memory from GPU back to CPU and store it in host_a
     int host_a[LIMIT];
     cudaMemcpy(host_a, device_a, LIMIT * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    cudaFree(device_a);
 
     // Run accumulate sum on CPU
-    // TODO parallel reduce on GPU.
     cout << accumulate(host_a, host_a + LIMIT, 0) << endl;
 
+    // TODO parallel reduce on GPU.
 }
