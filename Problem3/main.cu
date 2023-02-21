3b3d20772fdc0f868d7e4c3a8153396c77aca01b
bunnyDrug <1652666+bunnyDrug@users.noreply.github.com>
2023-02-21 00:52:42 +0000

    commit before reinstall.

---
+++ Problem3/main.cu
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+using namespace std;
+
+#define phi 1.61803398
+
+// I do not think this solution can be done more efficiently on the GPU
+int main() {
+    // create numbers
+
+    // check for primality
+
+    // result - check for factors
+
+    // get max from result.
+}
+    2     3     4     5     6
+ [i%2=1] [i%2] [i%2] [i%2] [i%2]
+    |      |     x     |     x
+
+    thread
