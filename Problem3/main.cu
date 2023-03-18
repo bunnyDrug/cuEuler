c22ccb9f5c88a7b9097150be3b88c41cc283ea30
bunnydrug <1652666+bunnyDrug@users.noreply.github.com>
2023-03-18 19:34:15 +0000

    a working prime sieve, but it was only working for smaller numbers within an max int range - this is my attempt at making this work for larger number. It doesn't work due to memory problems.

--- Problem3/main.cu
+++ Problem3/main.cu
@@ -1,41 +1,85 @@
 #include <iostream>
 #include <cuda_runtime.h>
+#include <math.h>
+
+
+void printPrimes(double *host_a, double size);
 
 using namespace std;
 
-#define N 10
+__global__ void createSieve(double *x, double N) {
+    for (int tid = blockIdx.x * blockDim.x + threadIdx.x; tid < N; tid += blockDim.x * gridDim.x) {
 
-__global__ void createSieve(int* x) {
-    int tid = threadIdx.x;
 
-    int start = 2;
+        // we are not interested in the 0 and 1 as they are not prime
+        if (tid == 1 || tid == 0) {
+            continue;
+        } else {
 
-sieve:
-    if (x[tid] != 0 && x[tid] % start == 0) {
-        x[tid] = 0;
-    }
-    cudaDeviceSynchronize();
-    for (int i = 0; i < N; i ++) {
-        if (x[i] > start && x[i] != 0) {
-            start = x[i];
-            goto sieve;
+            // let's begin at 2
+            double sieve = 2;
+
+            x[tid] = tid;
+
+            // Sieve all 2's
+            if (x[tid] != 0 && fmod(x[tid], sieve) == 0 && x[tid] != sieve) {
+                x[tid] = 0;
+            }
+
+            while (true) {
+                if (tid > sieve && x[tid] != 0) {
+                    sieve++;
+
+                    if (x[tid] != 0 && fmod(x[tid], sieve) == 0 && x[tid] != sieve) {
+                        x[tid] = 0;
+                        break;
+                    }
+                } else {
+                    break;
+                }
+
+            }
         }
     }
 }
 
 int main() {
+    double limit = 600851475143;
+
     // allocate memory on the GPU
-    int *device_a;
-    cudaMalloc((void **) &device_a, N * sizeof(int));
+    double *device_a;
+    cudaMalloc((void **) &device_a, limit * sizeof(double));
 
     // run kernel
-    createSieve<<<1, 10>>>(device_a);
+    createSieve<<<1, 1>>>(device_a, limit);
 
     // Copy memory from GPU back to CPU and store it in host_a
-    int host_a[N];
-    cudaMemcpy(host_a, device_a, N * sizeof(int), cudaMemcpyDeviceToHost);
+    double *host_a;
+    cudaMemcpy(host_a, device_a, limit * sizeof(double), cudaMemcpyDeviceToHost);
     cudaDeviceSynchronize();
     cudaFree(device_a);
 
-    cout << host_a << endl;
+
+    double halfwayPoint = sqrt(limit);
+
+    cout << "sqrt was: " << halfwayPoint << endl;
+    cout << "There can be at most one other prime factor greater than " << halfwayPoint << endl;
+
+    bool neverFound = true;
+    for (int i = ceil(halfwayPoint); i < limit; i++) {
+        if (host_a[i] != 0 && fmod(limit, host_a[i]) == 0) {
+            cout << "Highest Prime is " << i << endl;
+            neverFound = false;
+            break;
+        }
+    }
+    if (neverFound) {
+        for (int i = ceil(halfwayPoint); i < limit; i--) {
+            if (host_a[i] != 0 && fmod(limit, host_a[i]) == 0) {
+                cout << "Highest Prime is " << i << endl;
+                break;
+            }
+        }
+    }
 }
+
